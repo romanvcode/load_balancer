b634bb9d9098650a3c0e93815f49d7c4716b5e7b
romanvcode <roman.v.ua@gmail.com>
2024-10-26 16:47:11 +0300

    Created Models and DbContext

---
+++ LoadBalancer.WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using LoadBalancer.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoadBalancer.WebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<TaskState> TaskStates { get; set; }
+    }
+}
---
+++ LoadBalancer.WebAPI/Models/TaskState.cs
@@ -0,0 +1,11 @@
+﻿namespace LoadBalancer.WebAPI.Models
+{
+    public class TaskState
+    {
+        public int Id { get; set; }
+        public double Progress { get; set; }
+        public string State { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string Result { get; set; }
+    }
+}
